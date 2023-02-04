8617583a539e6a9367f1dd89120b24faa5f96b08
bxste <97215500+bxste@users.noreply.github.com>
2023-02-04 22:11:25 +0800

    6 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
 import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
 import { ReferencesComponent } from './references/references.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'references', component: ReferencesComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'PagenotfoundComponent', component: PagenotfoundComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   declarations: [
     AppComponent,
     HelloComponent,
@@ -18,6 +32,8 @@ import { SkillsComponent } from './skills/skills.component';
     EducationComponent,
     ReferencesComponent,
     SkillsComponent,
+    PagenotfoundComponent,
+    HomeComponent,
   ],
   bootstrap: [AppComponent],
 })
